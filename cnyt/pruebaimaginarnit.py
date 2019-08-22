4d04e2b46fa270251cbed043cd645defe8d26e9f
julitom1 <54115327+julitom1@users.noreply.github.com>
2019-08-22 10:43:25 -0500

    Add files via upload

--- cnyt/pruebaimaginarnit.py
+++ cnyt/pruebaimaginarnit.py
@@ -10,6 +10,12 @@ class unit_imaginar(unittest.TestCase):
         def test_resta(self):
                 self.assertEqual(imaginar.resta((2,5),(3,2)),(-1,3))
 
+        def test_multiplicacion(self):
+                self.assertEqual(imaginar.multiplicacion((2,3),(4, 5)),(-7,22))
+
+        def test_division(self):
+                self.assertEqual(imaginar.division((3,2),(-1, 2)),(0.2,-1.6))
+
         def test_fase(self):
 
                 self.assertEqual(imaginar.fase((3,2)),0.59)
@@ -25,6 +31,9 @@ class unit_imaginar(unittest.TestCase):
 
         def test_polar_cartesiano(self):
                 self.assertEqual(imaginar.polar_cartesiano((3,60)),(-2.86, -0.91))
+
+        def test_conjugado(self):
+                self.assertEqual(imaginar.conjugado((6,-5)),(6,5))
 
     
      
