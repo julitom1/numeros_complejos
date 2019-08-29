d88e152a5021c09f232293ce291af9c582b7e635
julitom1 <54115327+julitom1@users.noreply.github.com>
2019-08-29 08:45:39 -0500

    Add files via upload

--- cnyt/calculadora_matrices.py
+++ cnyt/calculadora_matrices.py
@@ -1,10 +1,26 @@
 from sys import stdin
 import imaginar
+def producto_escalar(c,v1):
+    for x in range(len(v1)):
+        v1[x]=imaginar.multiplicacion(c,v1[x])
+    return v1
+
 def adicion_vectores(matriz1,matriz2):
+    
+    matriz3,matriz4=[],[]
+    matriz3.append(matriz1)
+    matriz4.append(matriz2)
+    return adicion_matrices(matriz3,matriz4)
+
+def inversa_vectores(matriz1):
+    
+    for x in range(len(matriz1)):
+        matriz1[x]=(-matriz1[x][0],-matriz1[x][1])
+    return matriz1
+
+def adicion_matrices(matriz1,matriz2):
     try:
         rta=[]
-        matriz1=[[(2,1),(0,0),(1,3)],[(3,2),(4,3),(5,2)]]
-        matriz2=[[(2,1),(0,0),(1,3)],[(3,2),(4,3),(5,2)]]
         n=len(matriz1[0])
         for x in range(len(matriz1)):
             lis=[]
@@ -15,23 +31,20 @@ def adicion_vectores(matriz1,matriz2):
         return rta
     except:
         return "La suma no tiene las mismas dimensiones"
-def inversion_vectores(matriz1):
+def inversa(matriz1):
     #try:
         n=len(matriz1[0])
-        matriz2=matriz_adjunta(matriz1)
-        matriz2=transpuesta(matriz2)
-        adj=cofactores(matriz1)
+        matriz2=transpuesta(adjunta(matriz1))
+        det=determinante(matriz1)
         lis=[]
         for x in range(len(matriz2)):
             lista=[]
             for y in range(len(matriz2)):
-                lista.append(imaginar.division((matriz2[x][y],adj)))
+                lista.append(imaginar.division(matriz2[x][y],det))
             lis.append(lista)
         return lis
-        
-        
     #except:print("no tiene las mismas dimensiones")
-def multiplicacion_escalar(escalar,matriz):
+def multiplicacion_escalar_matrices(escalar,matriz):
     matriz2=[]
     n=len(matriz)
     for x in range(n):
@@ -46,11 +59,12 @@ def transpuesta(matriz1):
     for x in range(len(matriz1[0])):
         lis=[]
         for y in range(len(matriz1)):
-            lis.append(matriz1[y][x])
+            z=matriz1[y][x]
+            lis.append(z)
         matriz2.append(lis)
     return matriz2
 
-def matriz_conjugada(matriz1):
+def conjugada(matriz1):
         n=len(matriz1)
         for x in range(n):
                 for y in range(n):
@@ -58,34 +72,49 @@ def matriz_conjugada(matriz1):
         return matriz1
 
 
-def matriz_adjunta(matriz1):
+def adjunta(matriz1):
     lista=[]
     for x in range(len(matriz1)):
         lis=[]
         for y in range(len(matriz1)):
-            lis.append(cofactores(matriz1,x,y))
+            k=determinante(cofactores(matriz1,x,y))
+            if (x+y)%2!=0:
+                k=imaginar.multiplicacion((-1,0),k)
+            lis.append(k)
         lista.append(lis)
     return lista
 
 #def matriz_accion():
 
-def norma(matriz):
-    norma = multiplicacion_matriz(matriz*matriz)
-    for x in range(len(norma)):
-        for y in range(len(norma)):
-            norma[x][y]=raiz_matriz(norma[x][y])
-    return norma
+#def norma(matriz):
+  #  matriz2=
+    #matriz2=multiplicacion(matriz)
+    #return determinante(matriz2)
 
 #def distancia_matrices():
     
 def matriz_unitaria(matriz1):
-        matriz2=matriz1*transpuesta(matriz1)
+        matriz3=transpuesta(matriz1)
+        matriz3=conjugada(matriz3)
+        matriz2=[]
+        matriz1=multiplicacion_matriz(matriz1,matriz3)
+        n=matriz1[0][0]
+        for x in range(len(matriz1)):
+            lis=[]
+            for y in range(len(matriz1)):
+                if x==y:
+                    lis.append((1,0))
+                    matriz1[x][y]=imaginar.division(matriz1[x][y],n)
+                else:
+                    lis.append((0,0))
+            matriz2.append(lis)
         return matriz2==matriz1
+    
 def matriz_hermitiana(matriz):
         matriz2=transpuesta(matriz)
-        print(matriz2)
-        matriz2=matriz_conjugada(matriz2)
-        return  matriz==matriz2 
+        matriz2=conjugada(matriz2)
+        return  matriz==matriz2
+    
 def producto_tensor(m1,m2):
     lista=[]
     for x in range(len(m1)):
@@ -101,40 +130,41 @@ def cofactores(matriz1,i,j):
     negativos=0
     n=len(matriz1)
     matriz2=[]
-    if len(matriz1)>2:
-        for x in range(n):
-            m=[]
-            if x!=i:
-                for y in range(n):
-                    if y!=j:
-
-                        m.append(matriz1[x][y])
-                matriz2.append(m)
-        matriz1=matriz2
-    return matriz1
+    for x in range(n):
+        m=[]
+        if x!=i:
+            for y in range(n):
+                if y!=j:
+                    m.append(matriz1[x][y])
+            matriz2.append(m)
+    return matriz2
                 
-    
 def determinante(matriz1):
-    while len(matriz1)>2:
-        matriz1=multiplicacion_escalar(matriz1[0][0],cofactores(matriz1,1,1))
-        print(matriz1)
-    print(matriz1)
-    r1=imaginar.multiplicacion(matriz1[0][0],matriz1[1][1])
-    return imaginar.resta(r1,imaginar.multiplicacion(matriz1[1][0],matriz1[0][1]))
-    
-def raiz_matriz(valor,y):
-    mod=cartesiano_polar(valor)
-    rta= mod[0]**(1/y),mod[1]/2
-    return rta
+    sumar=0,0
+    if len(matriz1)>2:
+        for x in range(len(matriz1)):
+            matriz2=cofactores(matriz1,x,0)
+            rta=imaginar.multiplicacion(matriz1[x][0],determinante(matriz2))
+            if x%2!=0:
+                rta=imaginar.multiplicacion((-1,0),rta)
+            sumar=imaginar.suma(sumar,rta)
+        return sumar
+    if len(matriz1)==2:
+        r1=imaginar.multiplicacion(matriz1[0][0],matriz1[1][1])
+        r2=imaginar.resta(r1,imaginar.multiplicacion(matriz1[0][1],matriz1[1][0]))
+    else:
+        r2=matriz1[0][0]
+    return r2
             
 def multiplicacion_matriz(matriz1,matriz2):
     lista=[]
     for x in range(len(matriz1)):
         lis=[]
-        for y in range(len(matriz2)):
+        for y in range(len(matriz2[0])):
             sumar=(0,0)
-            for z in range(len(matriz1[0])):
+            for z in range(len(matriz2)):
                 k=imaginar.multiplicacion(matriz1[x][z],matriz2[z][y])
-                lis.append(imaginar.sumar(sumar,k))
+                sumar=imaginar.suma(sumar,k)
+            lis.append(sumar)
         lista.append(lis)
     return lista
--- cnyt/prueba_calculadora.py
+++ cnyt/prueba_calculadora.py
@@ -6,14 +6,49 @@ import imaginar
 class unit_imaginar(unittest.TestCase):
         
         def test_adicion_vectores(self):
-                self.assertEqual(calculadora_matrices.adicion_vectores([[(2,1),(0,0),(1,3)]],[[(3,2),(4,3),(5,2)]]),  [[(4, 2), (0, 0), (2, 6)], [(6, 4), (8, 6), (10, 4)]])
+                self.assertEqual(calculadora_matrices.adicion_vectores([(3,1),(2,1),(4,2)],[(3,2),(4,3),(5,2)]),[[(6, 3), (6, 4), (9, 4)]] )
 
-     #   def test_inversion_vectores(self):
-       #         self.assertEqual(calculadora_matrices.inversion_vectores([[(2,1),(0,0),(1,3)]],[[(3,2),(4,3),(5,2)]]),  [[(4, 2), (0, 0), (2, 6)], [(6, 4), (8, 6), (10, 4)]])
-        def test_matriz_adjunta(self):
-                self.assertEqual(calculadora_matrices.matriz_adjunta([[(1,0),(0,0),(0,0)],[(0,0),(0,1),(0,2)],[(1,-1),(0,0),(1,1)]]),[[(-1, 1), (2, 2), (-1, -1)], [(0, 0), (1, 1), (0, 0)], [(0, 0), (0, -2), (0, 1)]])
+        def test_inversa(self):
+                 self.assertEqual(calculadora_matrices.inversa_vectores([(1,-1),(0,0),(1,1)]),[(-1, 1), (0, 0), (-1, -1)])
 
-        def test_determinante(self):
-                self.assertEqual(calculadora_matrices.determinante([[(4,2),(2,0),(-3,-2)],[(1,0),(0,5),(-2,0)],[(1,0),(-2,-5),(4,0)]]),(-36, 32)) 
-if True:
-    unittest.main()
+        def test_multiplicacion_escalar(self):
+                self.assertEqual(calculadora_matrices.producto_escalar((2,4),[(2,-9),(0,0),(1,-5)]),[(40, -10), (0, 0), (22, -6)])
+
+        def test_adicion_matrices(self):
+                self.assertEqual(calculadora_matrices.adicion_matrices([[(2,1),(0,0),(1,3)],[(1,0),(2,4),(3,5)]],[[(3,2),(4,3),(5,2)],[(3,2),(4,3),(5,2)]]),  [[(5, 3), (4, 3), (6, 5)], [(4, 2), (6, 7), (8, 7)]])
+
+        def test_inversa_matrices(self):
+                self.assertEqual(calculadora_matrices.inversa([[(1,0),(0,0),(0,0)],[(0,0),(0,1),(0,2)],[(1,-1),(0,0),(1,1)]]),[[(1.0, 0.0), (0.0, 0.0), (0.0, 0.0)], [(0.0, -2.0), (0.0, -1.0), (-1.0, 1.0)], [(0.0, 1.0), (0.0, 0.0), (0.5, -0.5)]])
+
+        def test_multiplicacion_escalar_matrices(self):
+            self.assertEqual(calculadora_matrices.multiplicacion_escalar_matrices((2,4),[[(2,-9),(0,0),(1,-5)],[(3,0),(0,0),(0,3)],[(5,2),(-1,-3),(1,0)]]),[[(40, -10), (0, 0), (22, -6)], [(6, 12), (0, 0), (-12, 6)], [(2, 24), (10, -10), (2, 4)]])
+
+        def test_transpuesta(self):
+            self.assertEqual(calculadora_matrices.transpuesta([[(2,-9),(0,0),(1,-5)],[(3,0),(0,0),(0,3)],[(5,2),(-1,-3),(1,0)]]),[[(2, -9), (3, 0), (5, 2)], [(0, 0), (0, 0), (-1, -3)], [(1, -5), (0, 3), (1, 0)]])
+        
+        def test_conjugado(self):
+            self.assertEqual(calculadora_matrices.conjugada([[(2,1),(3,0),(-1,4)],[(4,-1),(5,0),(17,-2)],[(1,0),(3,-1),(1,3)]]),[[(2, -1), (3, 0), (-1, -4)], [(4, 1), (5, 0), (17, 2)], [(1, 0), (3, 1), (1, -3)]])
+
+        def test_adjunta(self):
+               self.assertEqual(calculadora_matrices.adjunta([[(1,0),(0,0),(0,0)],[(0,0),(0,1),(0,2)],[(1,-1),(0,0),(1,1)]]),[[(-1, 1), (2, 2), (-1, -1)], [(0, 0), (1, 1), (0, 0)], [(0, 0), (0, -2), (0, 1)]])
+
+        #def test_accion(self):
+
+
+        #def test_norma_matricial:
+
+           #def test_distancia_entrematrices 
+
+        def test_unitaria(self):
+            self.assertEqual(calculadora_matrices.matriz_unitaria([[(1,0),(-1,1)],[(1,1),(1,0)]]),True)
+
+        def test_Hermitian(self):
+                self.assertEqual(calculadora_matrices.matriz_hermitiana([[(3,0),(2,1)],[(2,-1),(1,0)]]),True)          
+
+        def test_producto_tensor(self):
+                self.assertEqual(calculadora_matrices.producto_tensor([[(2,1),(3,0),(-1,4)],[(4,-1),(5,0),(17,-2)],[(1,0),(3,-1),(1,3)]],[[(4,2),(2,0),(-3,-2)],[(1,0),(0,5),(-2,0)],[(1,0),(-2,0),(4,0)]]),[[(6, 8), (4, 2), (-4, -7), (2, 1), (-5, 10), (-4, -2), (2, 1), (-4, -2), (8, 4)], [(12, 6), (6, 0), (-9, -6), (3, 0), (0, 15), (-6, 0), (3, 0), (-6, 0), (12, 0)], [(-12, 14), (-2, 8), (11, -10), (-1, 4), (-20, -5), (2, -8), (-1, 4), (2, -8), (-4, 16)], [(18, 4), (8, -2), (-14, -5), (4, -1), (5, 20), (-8, 2), (4, -1), (-8, 2), (16, -4)], [(20, 10), (10, 0), (-15, -10), (5, 0), (0, 25), (-10, 0), (5, 0), (-10, 0), (20, 0)], [(72, 26), (34, -4), (-55, -28), (17, -2), (10, 85), (-34, 4), (17, -2), (-34, 4), (68, -8)], [(4, 2), (2, 0), (-3, -2), (1, 0), (0, 5), (-2, 0), (1, 0), (-2, 0), (4, 0)], [(14, 2), (6, -2), (-11, -3), (3, -1), (5, 15), (-6, 2), (3, -1), (-6, 2), (12, -4)], [(-2, 14), (2, 6), (3, -11), (1, 3), (-15, 5), (-2, -6), (1, 3), (-2, -6), (4, 12)]])
+unittest.main()
+
+        
+        
+
